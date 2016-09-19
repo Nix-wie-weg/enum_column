80aec10063b0577a131d88e52134326ab51f59b2
Sven Winkler <sven.winkler@nix-wie-weg.de>
2016-09-19 11:18:12 +0200

    #19648: Merge upstream changes by jewlr/enum_column

--- lib/enum/enum_adapter.rb
+++ lib/enum/enum_adapter.rb
@@ -1,22 +1,49 @@
 # This module provides all the column helper methods to deal with the
 # values and adds the common type management code for the adapters.
 
-# try rails 3.1, then rails 3.2+, mysql column adapters
-column_class = if defined? ActiveRecord::ConnectionAdapters::Mysql2Column
-  ActiveRecord::ConnectionAdapters::Mysql2Column
-elsif defined? ActiveRecord::ConnectionAdapters::MysqlColumn
-  ActiveRecord::ConnectionAdapters::MysqlColumn
-elsif defined? ActiveRecord::ConnectionAdapters::Mysql2Adapter::Column
-  ActiveRecord::ConnectionAdapters::Mysql2Adapter::Column
-elsif defined? ActiveRecord::ConnectionAdapters::MysqlAdapter::Column
-  ActiveRecord::ConnectionAdapters::MysqlAdapter::Column
-else
-  ObviousHint::NoMysqlAdapterFound
+adapter_class =
+  if defined? ActiveRecord::ConnectionAdapters::Mysql2Adapter
+    ActiveRecord::ConnectionAdapters::Mysql2Adapter
+  elsif defined? ActiveRecord::ConnectionAdapters::MysqlAdapter
+    ActiveRecord::ConnectionAdapters::MysqlAdapter
+  end
+
+if adapter_class
+  adapter_class.class_eval do
+    protected
+    if instance_methods.include?(:initialize_type_map)
+      def initialize_type_map_with_enum_types(m)
+        initialize_type_map_without_enum_types(m)
+        m.register_type(/enum/i) do |sql_type|
+          limit = sql_type
+            .sub(/^enum\('(.+)'\)/i, '\1')
+            .split("','")
+            .map(&:intern)
+          ActiveRecord::Type::Enum.new(limit: limit)
+        end
+      end
+      alias_method_chain :initialize_type_map, :enum_types
+    end
+  end
 end
 
-column_class.module_eval do
+# Try Rails 3.1, then Rails 3.2+, then mysql column adapters
+column_class =
+  if defined? ActiveRecord::ConnectionAdapters::Mysql2Column
+    ActiveRecord::ConnectionAdapters::Mysql2Column
+  elsif defined? ActiveRecord::ConnectionAdapters::MysqlColumn
+    ActiveRecord::ConnectionAdapters::MysqlColumn
+  elsif defined? ActiveRecord::ConnectionAdapters::Mysql2Adapter::Column
+    ActiveRecord::ConnectionAdapters::Mysql2Adapter::Column
+  elsif defined? ActiveRecord::ConnectionAdapters::MysqlAdapter::Column
+    ActiveRecord::ConnectionAdapters::MysqlAdapter::Column
+  else
+    ObviousHint::NoMysqlAdapterFound
+  end
 
-  alias __klass_enum klass
+column_class.module_eval do
+  alias_method :__klass_enum, :klass
+
   # The class for enum is Symbol.
   def klass
     if type == :enum
@@ -26,48 +53,113 @@ column_class.module_eval do
     end
   end
 
-  alias __type_cast_enum type_cast
-  # Convert to a symbol.
-  def type_cast(value)
+  if instance_methods.include?(:extract_default)
+    alias_method :__extract_default_enum, :extract_default
+
+    def extract_default
+      @default = @default.intern if type == :enum && @default.present?
+      __extract_default_enum
+    end
+  end
+
+  def __enum_type_cast(value)
     if type == :enum
-      value.is_a?(Symbol) ? value : string_to_valid_enum(value)
+      self.class.value_to_symbol(value)
     else
       __type_cast_enum(value)
     end
   end
 
+  if instance_methods.include?(:type_cast_from_database)
+    alias_method :__type_cast_enum, :type_cast_from_database
+
+    # Convert to a symbol.
+    def type_cast_from_database(value)
+      __enum_type_cast(value)
+    end
+  elsif instance_methods.include?(:type_cast)
+    alias_method :__type_cast_enum, :type_cast
+
+    def type_cast(value)
+      __enum_type_cast(value)
+    end
+  end
+
+  # Deprecated in Rails 4.1
+  if instance_methods.include?(:type_cast_code)
+    alias_method :__type_cast_code_enum, :type_cast_code
+
+    # Code to convert to a symbol.
+    def type_cast_code(var_name)
+      if type == :enum
+        "#{self.class.name}.value_to_symbol(#{var_name})"
+      else
+        __type_cast_code_enum(var_name)
+      end
+    end
+  end
+
+  class << self
+    # Safely convert the value to a symbol.
+    def value_to_symbol(value)
+      case value
+      when Symbol
+        value
+      when String
+        value.empty? ? nil : value.intern
+      end
+    end
+  end
+
   private
 
-  alias __simplified_type_enum simplified_type
-  # The enum simple type.
-  def simplified_type(field_type)
-    if field_type =~ /enum/i
-      :enum
-    else
-      __simplified_type_enum(field_type)
+  # Deprecated in Rails 4.2
+  if private_instance_methods.include?(:simplified_type)
+    alias_method :__simplified_type_enum, :simplified_type
+
+    # The enum simple type.
+    def simplified_type(field_type)
+      if field_type =~ /enum/i
+        :enum
+      else
+        __simplified_type_enum(field_type)
+      end
     end
   end
 
-  alias __extract_limit_enum extract_limit
-  def extract_limit(sql_type)
-    if sql_type =~ /^enum/i
-      sql_type.sub(/^enum\('(.+)'\)/i, '\1').split("','").map { |v| v.intern }
-    else
-      __extract_limit_enum(sql_type)
+  # Deprecated in Rails 4.2
+  if private_instance_methods.include?(:extract_limit)
+    alias_method :__extract_limit_enum, :extract_limit
+
+    def extract_limit(sql_type)
+      if sql_type =~ /^enum/i
+        sql_type.sub(/^enum\('(.+)'\)/i, '\1').split("','").map(&:intern)
+      else
+        __extract_limit_enum(sql_type)
+      end
     end
   end
+end
 
-  def string_to_valid_enum(str)
-    @valid_strings_filter ||= Hash[enum_valid_string_assoc]
-    @valid_strings_filter[str]
+# Rails 4.2 type annotations
+if defined? ActiveRecord::Type::Value
+  module ActiveRecord
+    module Type
+      class Enum < Value # :nodoc:
+        def type
+          :enum
+        end
+
+        def type_cast_for_database(value)
+          value.to_s if value.present?
+        end
+
+        private
+
+        def cast_value(value)
+          value.to_sym if value.present?
+        end
+      end
+    end
   end
-
-  def string_to_valid_enum_hash_code
-    Hash[enum_valid_string_assoc].to_s
-  end
-
-  def enum_valid_string_assoc
-    limit.map(&:to_s).zip(limit)
-  end
-
 end
